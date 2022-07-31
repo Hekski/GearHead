1ad214b42cb2fdacec9263c8b94f9adaf203aa17
LesHank <henrik.berglund@gmail.com>
2022-08-01 01:49:16 +0200

    main layout

--- src/router.js
+++ src/router.js
@@ -6,17 +6,20 @@ import Contact from "./components/contact";
 import PostsComponent from "./components/posts";
 
 import Header from "./components/header/header";
+import MainLayout from "./layouts/mainLayout"
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/article/:id" element={<PostsComponent />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="*" element={<MissingPage />}></Route>
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/article/:id" element={<PostsComponent />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="*" element={<MissingPage />}></Route>
+        </Routes>
+      </MainLayout>
     </BrowserRouter>
   );
 };
