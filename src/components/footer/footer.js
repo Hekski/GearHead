391cb9f50b01381d7a0e448a73d02b09a0814b7f
LesHank <henrik.berglund@gmail.com>
2022-08-05 15:02:33 +0200

    footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="d-flex bg-dark text-muted flex-column justify-content-center">
+        <p>Henrik Berglund</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
