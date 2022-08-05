115031acaf648f6ff466e965172db95d452650fa
LesHank <henrik.berglund@gmail.com>
2022-08-05 09:56:52 +0200

    post details showing detail

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Moment from "react-moment";
+
+import { requestById } from "../../store/utils/thunk";
+import posts from "../../store/reducers/posts";
 
 const PostsComponent = () => {
-  return <div>Posts</div>;
+  const postDetail = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  let param = useParams();
+
+  console.log(postDetail);
+  useEffect(() => {
+    dispatch(requestById(param.id));
+  }, []);
+
+  return (
+    <>
+      {posts.requestById ? (
+        <div className="container text-light">
+          <h1 className="display-2">{postDetail.postById.title}</h1>
+          <img src={postDetail.postById.image} />
+          <h4 className="mt-3">{postDetail.postById.excerpt}</h4>
+          <div
+            className="mt-3"
+            dangerouslySetInnerHTML={{
+              __html: postDetail.postById.content,
+            }}
+          ></div>
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default PostsComponent;
