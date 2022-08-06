edee0b9426d9ff1ef74e4c7f4b110a09077ce2f6
LesHank <henrik.berglund@gmail.com>
2022-08-06 23:21:51 +0200

    newsletter jsx

--- src/components/utils/newsLetter.js
+++ src/components/utils/newsLetter.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import { useRef, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const NewsLetter = () => {
-  return <div className="text-light">NewsLetter</div>;
-}
+  const textInput = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = textInput.current.value;
+    console.log(value);
+  };
+
+  return (
+    <div className="container">
+      <div className="form">
+        <Form onSubmit={handleSubmit} className="d-flex">
+          <Form.Group>
+            <Form.Control
+              className="rounded-0"
+              type="text"
+              placeholder="Join our newsletter"
+              name="email"
+              ref={textInput}
+            />
+          </Form.Group>
+          <Button className="btn-warning bg-gradient rounded-0" type="submit">
+            Add
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
 export default NewsLetter;
